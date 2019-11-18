01df2867d1ce4d1ef3ab4d1d13a6b3b4530d59f8
KentoAdachi <bp16001@shibaura-it.ac.jp>
2019-11-18 15:35:04 +0900

    Add files via upload

--- src/distributionGenerator/GeneratorRandom.java
+++ src/distributionGenerator/GeneratorRandom.java
@@ -3,12 +3,12 @@ package distributionGenerator;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class GeneratorRandom implements IGenerator {
+public class GeneratorRandom extends DistributionGenerator implements IGenerator {
 
-	int max_units_;
-	AllocationMap allocationMap_;
-
-	ArrayList<Hardware> nodes_;
+//	int max_units_;
+//	AllocationMap allocationMap_;
+//	ArrayList<Hardware> nodes_;
+//	double total_performance_;
 
 	public GeneratorRandom(ArrayList<Hardware> nodes, int w, int h) {
 		nodes_ = nodes;
@@ -22,7 +22,8 @@ public class GeneratorRandom implements IGenerator {
 		//割り当て結果の入ったリストを生成
 		ArrayList<Integer> omikuziList = new ArrayList<Integer>();
 		for (int i = 0; i < nodes_.size(); i++) {
-			for (int j = 0; j < nodes_.get(i).num_units_; j++) {
+//			for (int j = 0; j < nodes_.get(i).num_units_; j++) {
+			for (int j = 0; j < num_units_.get(i); j++) {
 				omikuziList.add(i + 1);
 			}
 		}
@@ -48,34 +49,36 @@ public class GeneratorRandom implements IGenerator {
 
 
 	//能力から割り当て数を決定
-	public void calc() {
-
-		double sum = 0;
-		for (Hardware hardware : nodes_) {
-			sum += hardware.performance_;
-
-		}
-
-		int tmp = 0;
-		//		for (Hardware hardware : nodes_) {
-		for (int i = 0; i < nodes_.size() - 1; i++) {
-			nodes_.get(i).num_units_left_ = Math.round((float) (max_units_ * nodes_.get(i).performance_ / sum));
-			nodes_.get(i).num_units_ = Math.round((float) (max_units_ * nodes_.get(i).performance_ / sum));
-
-			tmp += nodes_.get(i).num_units_left_;
-		}
-
-		nodes_.get(nodes_.size() - 1).num_units_left_ = max_units_ - tmp;
-		nodes_.get(nodes_.size() - 1).num_units_ = max_units_ - tmp;
-
-		//checker
-		tmp = 0;
-		for (Hardware hardware : nodes_) {
-			tmp += hardware.num_units_left_;
-		}
-		System.out.println("total units : " + tmp);
-
-	}
+//	public void calc() {
+//
+//		double sum = 0;
+//		for (Hardware hardware : nodes_) {
+//			sum += hardware.performance_;
+//
+//		}
+//		total_performance_ = sum;
+//
+//		int tmp = 0;
+//		//		for (Hardware hardware : nodes_) {
+//		for (int i = 0; i < nodes_.size() - 1; i++) {
+//			nodes_.get(i).num_units_left_ = Math.round((float) (max_units_ * nodes_.get(i).performance_ / sum));
+//			nodes_.get(i).num_units_ = Math.round((float) (max_units_ * nodes_.get(i).performance_ / sum));
+//
+//			tmp += nodes_.get(i).num_units_left_;
+//		}
+//
+//		nodes_.get(nodes_.size() - 1).num_units_left_ = max_units_ - tmp;
+//		nodes_.get(nodes_.size() - 1).num_units_ = max_units_ - tmp;
+//
+//		//checker
+//		tmp = 0;
+//		for (Hardware hardware : nodes_) {
+//			tmp += hardware.num_units_left_;
+////			System.out.println(hardware);
+//		}
+//		System.out.println("total units : " + tmp);
+//
+//	}
 
 	@Override
 	public AllocationMap getAllocationMap() {
