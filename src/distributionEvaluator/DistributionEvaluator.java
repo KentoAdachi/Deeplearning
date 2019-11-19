5473c5b2dd309e759ce759a9183202d6fa7a28ee
KentoAdachi <bp16001@shibaura-it.ac.jp>
2019-11-19 18:41:08 +0900

    Add files via upload

--- src/distributionEvaluator/DistributionEvaluator.java
+++ src/distributionEvaluator/DistributionEvaluator.java
@@ -1,15 +1,31 @@
 package distributionEvaluator;
 
+import java.util.ArrayList;
+
+import distributionGenerator.AllocationMap;
+import distributionGenerator.Hardware;
+
 public class DistributionEvaluator{
+
+	AllocationMap allocation_map_;
+	ArrayList<Hardware> nodes_;
+
+	public boolean enable_zero_padding_ = false;
+
+	public DistributionEvaluator(AllocationMap map,ArrayList<Hardware>nodes) {
+		allocation_map_ = map;
+		nodes_ = nodes;
+	}
 
 	//処理割り当て比率
 	public void evaluatePerformanceBallance() {
 
 	}
 
-	//通信データ量
-	public void evaluateDataAmount() {
+	//送受信データ量
+	public void evaluateTranslatedDataAmount() {
 
 	}
+
 
 }
